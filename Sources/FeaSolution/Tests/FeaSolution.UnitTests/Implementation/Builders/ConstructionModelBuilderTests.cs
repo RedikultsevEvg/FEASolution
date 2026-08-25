2ef08d7b2a6a422816cfde885d0253b86162789c
Sergey Petrov <srgpt@narod.ru>
2026-08-25 12:33:44 +0300

    Тест

---
+++ Sources/FeaSolution/Tests/FeaSolution.UnitTests/Implementation/Builders/ConstructionModelBuilderTests.cs
@@ -0,0 +1,42 @@
+﻿using FeaSolution.Core.Enums;
+using FeaSolution.Implementation.Builders;
+
+namespace FeaSolution.UnitTests.Implementation.Builders;
+
+[TestFixture]
+[TestOf(typeof(ConstructionModelBuilder))]
+public class ConstructionModelBuilderTests
+{
+    [Test]
+    public void CreateNew1D_SetsAllowedNodeTypeAndInitializesEmptyElements()
+    {
+        // Act
+        var model = ConstructionModelBuilder.CreateNew1D();
+
+        // Assert
+        Assert.That(model.AllowedNodeType.Dimension, Is.EqualTo(Dimensional.OneDimensional));
+        Assert.That(model.Elements, Is.Empty);
+    }
+
+    [Test]
+    public void CreateNew2D_SetsAllowedNodeTypeAndInitializesEmptyElements()
+    {
+        // Act
+        var model = ConstructionModelBuilder.CreateNew2D();
+
+        // Assert
+        Assert.That(model.AllowedNodeType.Dimension, Is.EqualTo(Dimensional.TwoDimensional));
+        Assert.That(model.Elements, Is.Empty);
+    }
+
+    [Test]
+    public void CreateNew3D_SetsAllowedNodeTypeAndInitializesEmptyElements()
+    {
+        // Act
+        var model = ConstructionModelBuilder.CreateNew3D();
+
+        // Assert
+        Assert.That(model.AllowedNodeType.Dimension, Is.EqualTo(Dimensional.ThreeDimensional));
+        Assert.That(model.Elements, Is.Empty);
+    }
+}
