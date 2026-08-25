eb628cd880e7f31df2052806ded0e0cdd375bde1
Sergey Petrov <srgpt@narod.ru>
2026-08-25 12:33:54 +0300

    Демопример. старт

--- Sources/FeaSolution/FeaSolution.Demo/Demo1/ConstructionModel.Demo1.cs
+++ Sources/FeaSolution/FeaSolution.Demo/Demo1/ConstructionModel.Demo1.cs
@@ -1,5 +1,4 @@
 ﻿using FeaSolution.Implementation.Builders;
-using FeaSolution.Implementation.ConstructionModels;
 
 namespace FeaSolution.Demo.Demo1;
 
@@ -7,11 +6,7 @@ internal static class ConstructionModelDemo
 {
     public static void BuildConstructionModel()
     {
-        var nodeType = ElementNodeTypeBuilder.CreateNew3D();
+        var model = ConstructionModelBuilder.CreateNew3D();
 
-        var model = new ConstructionModel
-        {
-            NodeType = nodeType
-        };
     }
 }
