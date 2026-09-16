48d727f90882e9101ddfecfd88d03a87d0a0aa80
Sergey Petrov <srgpt@narod.ru>
2026-09-16 11:07:23 +0300

    Add comments

--- Sources/FeaSolution/FeaSolution/Core/Types/DegreeOfFreedom.cs
+++ Sources/FeaSolution/FeaSolution/Core/Types/DegreeOfFreedom.cs
@@ -2,6 +2,9 @@ using FeaSolution.Core.Enums;
 
 namespace FeaSolution.Core.Types;
 
+/// <summary>
+/// Degree of freedom.
+/// </summary>
 public class DegreeOfFreedom
 {
     public Freedom Freedom { get; init; }
--- Sources/FeaSolution/FeaSolution/Core/Types/FiniteElementType.cs
+++ Sources/FeaSolution/FeaSolution/Core/Types/FiniteElementType.cs
@@ -1,9 +1,18 @@
 namespace FeaSolution.Core.Types;
 
+/// <summary>
+/// Finite element type.
+/// </summary>
 public class FiniteElementType
 {
+    /// <summary>
+    /// Collection of degree of freedom.
+    /// </summary>
     public required ICollection<DegreeOfFreedom> Freedoms { get; init; }
 
+    /// <summary>
+    /// Finite element node type.
+    /// </summary>
     public required ElementNodeType NodeType { get; init; }
 
     public required Action<MatrixValue> StiffnessMatrixCalculationMethod { get; init; }
